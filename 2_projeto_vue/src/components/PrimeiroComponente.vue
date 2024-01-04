64814d5498a69a7683c6bd53194b2d4db6a7eb28
Eladio Alves <eladiolink1@gmail.com>
2024-01-04 15:28:10 -0300

    Dados em Componentes

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,9 +1,19 @@
 <template>
-    <h1>Hello Vue!!</h1>
+    <div>
+        <h1>Hello Vue!!</h1>
+        <P>My name is {{ name }}</P>
+        <p>I work witrh {{proffission}}</p>
+    </div>
 </template>
 
 <script>
   export default{
-    name: "PrimeiroComponente"
+    name: "PrimeiroComponente",
+    data(){
+        return {
+            name: "Eladio",
+            proffission:"Programmer"
+        }
+    }
   }
 </script>
