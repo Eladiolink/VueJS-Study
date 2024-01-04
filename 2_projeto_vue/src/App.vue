9449fa54b5223b0750beb2efc0e7083c7af3fa2c
Eladio Alves <eladiolink1@gmail.com>
2024-01-04 15:44:56 -0300

    Life Cycle Hooks

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-    <PrimeiroComponenteVue/>
+    <div>
+      <PrimeiroComponenteVue/>
+      <LifeCycle/>
+    </div>
 </template>
 
 <script>
 import PrimeiroComponenteVue from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue';
   export default{
     name: "app",
     components:{
-      PrimeiroComponenteVue
+      PrimeiroComponenteVue,
+      LifeCycle
     }
   }
 </script>
---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>My name is: {{name}}</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "LifeCycle",
+        data(){
+            return {
+                name : "sla kekw"
+            }
+        },
+        created(){
+            setTimeout(()=>{
+                this.name = "Eladio"
+            }, 1000)
+           
+        },
+        mounted(){
+            setTimeout(()=>{
+                this.name =  'Pedoca'
+            }, 2000)  
+        }
+    }
+</script>
+
