a914986d4bfeb52251539f8ac2d0335bfd2fe7d3
Eladio Alves <eladiolink1@gmail.com>
2024-01-04 15:08:24 -0300

    Vue CLI e Components

---
+++ 2_projeto_vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <PrimeiroComponenteVue/>
+</template>
+
+<script>
+import PrimeiroComponenteVue from './components/PrimeiroComponente.vue'
+  export default{
+    name: "app",
+    components:{
+      PrimeiroComponenteVue
+    }
+  }
+</script>
