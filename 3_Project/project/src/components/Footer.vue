24ee8d44ff7fc27e738573fdf6c555ab4d07d479
Eladio Alves <eladiolink1@gmail.com>
2024-01-25 18:10:21 -0300

    Projeto

---
+++ 3_Project/project/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+   <footer id="footer">
+        <p>Make your Burguer &copy; 2024</p>
+   </footer>
+</template>
+
+<script>
+export default{
+    name:"Footer"
+}
+</script>
+
+<style scoped>
+
+#footer{
+    height: 100px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+</style>
